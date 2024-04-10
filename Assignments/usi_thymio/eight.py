adc1118f2da0dd5f3fbf6b3df63640e40ffe1d2e
Frenzoid <frenzoid@pm.me>
2024-04-10 17:51:48 +0200

    added new resources

---
+++ Assignments/usi_thymio/eight.py
@@ -0,0 +1,72 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+class EightController(Node):
+    def __init__(self):
+        super().__init__('eight_controller')
+
+        # Create a publisher to send velocity commands
+        self.velocity_publisher = self.create_publisher(Twist, 'cmd_vel', 10)
+
+        # Start State: circle_right then circle_left
+        self.state = 'circle_right'
+
+        # Setup speed and rads
+        self.speed = 0.2
+        self.rads = 1
+
+        # Setup time necessary to complete a circle
+        self.circle_time = 12
+
+        # Start the robot
+        self.control_loop()
+
+        # Create a timer to change the state every circle_time
+        self.create_timer(self.circle_time, self.control_loop)
+
+    def control_loop(self):
+        """ Control the robot based on the state """
+
+        if self.state == 'circle_right':
+            self.get_logger().info('Circle Right')
+            self.move(self.speed, -self.rads)
+            self.state = 'circle_left'
+        elif self.state == 'circle_left':
+            self.get_logger().info('Circle Left')
+            self.move(self.speed, self.rads)
+            self.state = 'circle_right'
+        
+
+    # --- Movements Functions ---
+    def move(self, speed=0.0, rads=0.0):
+        """ Move the robot with a given speed and turn """
+        velocity = Twist()
+        
+        # Forward Speed
+        velocity.linear.x = float(speed)
+
+        # Rotate speed
+        velocity.angular.z = float(rads)
+
+        self.velocity_publisher.publish(velocity)
+
+    def stop(self):
+        """ Stop the robot """
+        self.velocity_publisher.publish(Twist())
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    
+    # movemments based on states
+    node = EightController()
+
+    rclpy.spin(node)
+
+    # Detect ctrl+c and stop the robot
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
